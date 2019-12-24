d07560e8d0c4541e752a34ee784cdb3d11e1d3b3
Adrienne Walker <enne@quisquo.us>
2019-12-24 15:02:55 -0800

    add force reload plugin option

--- resources/user_config.js
+++ resources/user_config.js
@@ -2,6 +2,10 @@
 
 let UserConfig = {
   getUserConfigLocation: function(overlayName, callback) {
+    window.addOverlayListener('onForceReload', () => {
+      window.location.reload();
+    });
+
     callOverlayHandler({
       call: 'cactbotLoadUser',
       source: location.href,
